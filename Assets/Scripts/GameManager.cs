fc3be9ab659d905fa1453de6dd631eb433c21ffa
KambellT <kambellt@yahoo.com>
2018-09-21 15:44:02 +0100

    Added a timer function

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public List<GameObject> critterPrefabs;
+    public float critterSpawnFrequency = 1.0f;
+    public Score scoreDisplay; 
+
+    private float lastCritterSpawn = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        // check if it is time to spawn the next critter
+        float nextSpawnTime = lastCritterSpawn + critterSpawnFrequency;
+
+		if (Time.time >= lastCritterSpawn + critterSpawnFrequency)
+        {
+            // it is time
+
+            // choose a random critter to spawn
+            int critterIndex = Random.Range(0, critterPrefabs.Count);
+            GameObject prefabToSpawn = critterPrefabs[critterIndex];
+
+            // spawn the critter!
+            GameObject spawnedCritter = Instantiate(prefabToSpawn);
+
+            // get access to our critter script
+            Critter critterScript = spawnedCritter.GetComponent<Critter>();
+
+            // tell the scritter script the score object
+            critterScript.scoreDisplay = scoreDisplay;
+
+            // update the mosr recent critter spawn time to now
+            lastCritterSpawn = Time.time;
+        }
+	}
+}
